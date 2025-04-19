ef4e7ea1abac2bfb0c6f75dc773125fc5d5104a6
Akshat Jain <akjn11@gmail.com>
2025-04-19 21:20:29 +0530

    Use operator overloading for printing the config object

--- include/simpledb/config.h
+++ include/simpledb/config.h
@@ -4,12 +4,15 @@
 
 #ifndef CONFIG_H
 #define CONFIG_H
+#include <ostream>
 #include <__filesystem/filesystem_error.h>
 
 namespace config {
 
     struct Config {
         std::filesystem::path data_dir;
+
+        friend std::ostream & operator<<(std::ostream &os, const Config &obj);
     };
 
     void init_config();
--- src/config.cpp
+++ src/config.cpp
@@ -20,7 +20,6 @@ namespace config {
             return;
         }
 
-        // read from env
         const char* env_data_dir = std::getenv("SIMPLE_DB_DATA_DIR");
         if (env_data_dir != nullptr && std::strlen(env_data_dir) > 0) {
             config.data_dir = env_data_dir;
@@ -33,6 +32,8 @@ namespace config {
             std::cout << "Created data directory: " << config.data_dir << std::endl;
         }
 
+        std::cout << "Using config:\n" << config << std::endl;
+
         initialized = true;
     }
 
@@ -41,5 +42,16 @@ namespace config {
             init_config();
         }
         return config;
+    }
+
+    /*
+     * This is an example of operator overloading.
+     * This function overloads the stream insertion operator (`<<`).
+     * It dictates that when the compiler sees an expression like `std::cout << myConfigObject`,
+     * where the left operand is an output stream (like `std::cout`, which is a `std::ostream`)
+     * and the right operand is a `const Config&`, this function should be called.
+     */
+    std::ostream & operator<<(std::ostream &os, const Config &obj) {
+        return os << "data_dir: " << obj.data_dir;
     }
 }
