346fc04c3ebb6b3105ec57fa607915b517fc2362
Akshat Jain <akjn11@gmail.com>
2025-05-30 20:36:15 +0530

    Try to fix CI

--- src/parser.cpp
+++ src/parser.cpp
@@ -12,7 +12,8 @@
 
 namespace parser {
 
-    CommandType get_command_type(const std::string &query) {
+    CommandType get_command_type(const std::string &query)
+    {
         std::string command;
 
         // Create an std::stringstream object named 'ss'.
