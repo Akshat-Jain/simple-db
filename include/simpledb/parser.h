9746a1738621f4f23a8ebb26a4d6b153bbc63ad5
Akshat Jain <akjn11@gmail.com>
2025-07-12 23:19:18 +0530

    Migrate all other layers of the code into the antlr flow

--- include/simpledb/parser.h
+++ include/simpledb/parser.h
@@ -4,36 +4,20 @@
 
 #ifndef SIMPLEDB_PARSER_H
 #define SIMPLEDB_PARSER_H
-#include <optional>
-
+#include <variant>
+#include "simpledb/command.h"
 #include "simpledb/ast/ast.h"
-#include "command.h"
 
 namespace parser {
+    // A variant that can hold any of the possible parsed command types.
+    using CommandVariant = std::variant<command::CreateTableCommand,
+                                        command::DropTableCommand,
+                                        command::InsertCommand,
+                                        command::ShowTablesCommand,
+                                        ast::SelectCommand>;
 
-    template <typename T>
-    struct ParseResult {
-        std::optional<T> command;
-        std::optional<std::string> error_message;
+    std::optional<CommandVariant> parse_sql(const std::string& query);
 
-        // This is a helper method to make checking for success easy by doing if(result) instead of
-        // if(result.command.has_value()). The 'explicit' keyword prevents accidental implicit conversions to bool.
-        explicit operator bool() const { return command.has_value(); }
-    };
-
-    enum class CommandType { CREATE_TABLE, INSERT, SELECT, DROP_TABLE, SHOW_TABLES, UNKNOWN };
-
-    CommandType get_command_type(const std::string& query);
-
-    ParseResult<command::CreateTableCommand> parse_create_table(const std::string& query);
-
-    ParseResult<command::DropTableCommand> parse_drop_table(const std::string& query);
-
-    ParseResult<command::ShowTablesCommand> parse_show_tables(const std::string& query);
-
-    ParseResult<command::InsertCommand> parse_insert(const std::string& query);
-
-    ParseResult<ast::SelectCommand> parse_select(const std::string& query);
 }  // namespace parser
 
 #endif  // SIMPLEDB_PARSER_H
