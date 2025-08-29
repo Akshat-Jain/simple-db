d960ae5fd5993a3c7a71b7c362e2c0907a181aa8
Akshat Jain <akjn11@gmail.com>
2025-08-29 12:04:51 +0530

    Add Benchmark_SelectWhereClause_1M_Rows

--- benchmarks/benchmark.cpp
+++ benchmarks/benchmark.cpp
@@ -54,3 +54,49 @@ BENCHMARK(Benchmark_InsertSelect)
     })
     ->Iterations(3)
     ->ArgNames({"numRows"});
+
+static void Benchmark_SelectWhereClause_1M_Rows(benchmark::State& state) {
+    const int rowId = state.range(0);
+    std::filesystem::path test_data_dir = std::filesystem::temp_directory_path() / state.name();
+    setenv("SIMPLE_DB_DATA_DIR", test_data_dir.c_str(), 1);
+    config::init_config();
+    catalog::initialize(test_data_dir);
+
+    // Create table and insert rows
+    query_runner::QueryRunner::run_query("create table benchmark_table (id INT, name TEXT);");
+    for (int i = 1; i <= 1000000; ++i) {
+        query_runner::QueryRunner::run_query("insert into benchmark_table (id, name) values (" + std::to_string(i) +
+                                             ", 'Name" + std::to_string(i) + "');");
+    }
+
+    // Measure the size of the data file created.
+    std::filesystem::path table_file = test_data_dir / "benchmark_table.data";
+    if (std::filesystem::exists(table_file)) {
+        auto file_size = std::filesystem::file_size(table_file);
+        state.SetLabel("size = " + std::to_string(file_size) + " bytes");
+    }
+
+    double sum_select_query_time = 0.0;
+    for (auto _ : state) {
+        auto start_select = std::chrono::high_resolution_clock::now();
+        auto result = query_runner::QueryRunner::run_query(
+            "select * from benchmark_table where id = " + std::to_string(rowId) + ";");
+        benchmark::DoNotOptimize(result);
+        auto end_select = std::chrono::high_resolution_clock::now();
+        sum_select_query_time +=
+            std::chrono::duration_cast<std::chrono::duration<double>>(end_select - start_select).count();
+    }
+
+    // Drop the table
+    query_runner::QueryRunner::run_query("drop table benchmark_table;");
+
+    state.counters["SelectQueryTime_seconds"] =
+        benchmark::Counter(sum_select_query_time, benchmark::Counter::kAvgIterations);
+}
+
+BENCHMARK(Benchmark_SelectWhereClause_1M_Rows)
+    ->ArgsProduct({
+        {10000000}  // rowId
+    })
+    ->Iterations(3)
+    ->ArgNames({"rowId"});
